a29ed816bc122747461b34016aff1ca2bb77cb27
Eijs <eijs666@hotmail.com>
2022-01-19 19:42:56 +0100

    Shooting star done

--- portfolio/myproject/main.js
+++ portfolio/myproject/main.js
@@ -23,43 +23,48 @@ const lavaTexture = new THREE.TextureLoader().load("lava.jpg");
 const blueLavaTexture = new THREE.TextureLoader().load("blueLava.jpg");
 
 // Objects ---
-// Planet Ring
+// Planet Ring - Red Diamond
 const planetRingGeometry = new THREE.TorusGeometry(20, 3, 2.5, 100);
-const planetRingMaterial = new THREE.MeshStandardMaterial({ map: lavaTexture});
+const planetRingMaterial = new THREE.MeshStandardMaterial({ map: redDiamond});
 const planetRing = new THREE.Mesh(planetRingGeometry, planetRingMaterial);
-planetRing.position.set(15,7,-40);
+planetRing.position.set(15,-2,-40);
 planetRing.rotation.set(1.8,0.4,0);
 scene.add(planetRing);
 
-// Planet Ring 2 - Blue
+// Planet Ring 2 - BlueLava
 const planetRing2Mat = new THREE.MeshBasicMaterial({map: blueLavaTexture});
 var planetRing2 = new THREE.Mesh(planetRingGeometry, planetRing2Mat);
 planetRing2.position.set(-50, 30, -10);
 planetRing2.rotation.set(1.8,15,0);
 scene.add(planetRing2);
 
+// Planet Ring 3 - Lava
+const planetRing3Mat = new THREE.MeshBasicMaterial({map: lavaTexture});
+var planetRing3 = new THREE.Mesh(planetRingGeometry, planetRing3Mat);
+planetRing3.position.set(20,30,20);
+planetRing3.rotation.set(5,-0.6,0);
+scene.add(planetRing3);
 
-// Planet
+
+// Planet - Red Diamond
 const planetGeo = new THREE.SphereGeometry(12, 24, 24);
 const planetMat = planetRingMaterial;
 const planet = new THREE.Mesh(planetGeo, planetMat);
-planet.position.set(15,7,-40);
+planet.position.set(15,-2,-40);
 scene.add(planet);
 
-// Planet 2
+// Planet 2 - Blue Lava
 const planet2Mat = new THREE.MeshBasicMaterial({map: blueLavaTexture});
 var planet2 = new THREE.Mesh(planetGeo, planet2Mat);
 planet2.position.set(-50, 30, -10);
 scene.add(planet2);
 // RING NOTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 
-// Planet 2
-const planet3Mat = new THREE.MeshBasicMaterial({color: "gray" });
+// Planet 3 - Lava
+const planet3Mat = new THREE.MeshBasicMaterial({map: lavaTexture });
 var planet3 = new THREE.Mesh(planetGeo, planet3Mat);
-planet3.position.set(-10, 30, -10);
+planet3.position.set(20, 30, 20);
 scene.add(planet3);
-// COLOR RING NOTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-
 
 // Stars 
 var stars = [];
@@ -70,14 +75,39 @@ function createStar(){
   const star = new THREE.Mesh(starGeometry, starMaterial);
 
   // Random star position
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(200));
   // Sets random generated position to star position
   star.position.set(x, y, z);
   stars.push(star); // Push star 
   scene.add(star); // Add star to scene
 }
 // How many stars // Save x number of stars in array
-var stars = Array(20).fill().forEach(createStar);
+stars = Array(0).fill().forEach(createStar);
+
+// Shooting Star ---
+var starShot; // Global variable to reference animation
+var isStarShot = false;
+console.log(isStarShot);
+function shootingStar(){
+  randomNumber(); // Generate y float for this star
+  const shootGeo = new THREE.SphereGeometry(1, 24, 24);
+  const shootMat = new THREE.MeshStandardMaterial({ color: "white" });
+  const shootStar = new THREE.Mesh(shootGeo, shootMat);
+  // Start position
+  shootStar.position.set(-700,0,-200);
+
+  starShot = shootStar; // Fill the empty star with new created star
+  scene.add(starShot); //Display star
+
+  isStarShot = true;
+console.log(isStarShot);
+}
+// Get button reference
+const btnShoot = document.getElementById("btnShoot");
+// When button is clicked shoot a star
+btnShoot.addEventListener("click", shootingStar);
+shootingStar();
+
 
 // Lights ---
 const pointLight = new THREE.PointLight(0xffffff);
@@ -94,7 +124,7 @@ scene.background = spaceTexture;
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
 
-  camera.position.z = t * -0.01;
+  camera.position.z = (t * 1.5) * -0.01;
   camera.rotation.x = t * -0.0002;
  // camera.position.x = t * -0.0002;
 }
@@ -102,15 +132,35 @@ function moveCamera() {
 document.body.onscroll = moveCamera;
 moveCamera();
 
-//Animation Loop
+// Generate Random Number
+var randomNum;
+function randomNumber(){
+  const ranNum = Math.random() * (1.5 - -1) + -1;
+  randomNum = ranNum;
+}
+
+
+// Animation Loop ---
 function animate() {
   requestAnimationFrame(animate);
 
-  // Torus.rotation.x += 0.01; // Torus Animation
-  planetRing.rotateZ(0.001); // Planet Ring Animation
-  planet.rotateY(0.001); // Planet Animation
-  planet2.rotateY(0.001); // Planet Animation
+  // Planet Rings Animation
+  planetRing.rotateZ(0.001); 
+  planetRing2.rotateZ(-0.001);  
+  planetRing3.rotateZ(-0.001);
 
+  // Planets Animations
+  planet.rotateY(0.001);
+  planet2.rotateY(0.001);
+  planet3.rotateY(0.001);
+
+  if(isStarShot){
+    starShot.position.x += 5;
+   starShot.position.y += randomNum;
+  }else{
+    return;
+  }
+  
   for (let index = 0; index < 11; index++) {
    //const element = stars[index];
    //console.log(element); //TEST
